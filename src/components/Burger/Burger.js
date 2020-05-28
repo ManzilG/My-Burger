2a33e08d3e6af4ccf1a367b98d7127c3d63aa7b6
Manzil Gajurel <manzilgajurel@Manzils-Mac.local>
2020-05-28 13:26:05 -0500

    First commit with simple Burger.

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    //Way to transform a object of key value pairs to array.
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])]
+                .map((_,i) => {
+                    return <BurgerIngredient key={igKey + i} type = {igKey}/>
+                })
+        }).reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    //Flattening the array
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type = 'bread-top' />
+            {transformedIngredients}
+            <BurgerIngredient type = 'bread-bottom' />
+        </div>
+    );
+};
+
+export default burger;
